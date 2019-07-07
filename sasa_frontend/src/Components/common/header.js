797ffc7ce84e775408d9411261b497a5f538b014
RahalAmrith <42468441+RahalAmrith@users.noreply.github.com>
2019-07-07 22:18:42 +0530

    07 / 07 / 2019 - 10:18pm

---
+++ sasa_frontend/src/Components/common/header.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import "../../assets/common/Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Head_main">
+        <div className="head_topRow">
+          <div className="Head_smLinks">
+            <i className="fab fa-facebook-f" />
+            <i className="fab fa-twitter" />
+            <i className="fab fa-linkedin-in" />
+            <i className="fab fa-instagram" />
+          </div>
+        </div>
+        <div className="Head_nav">
+          <div className="Head_logo" />
+          <ul className="Head_navList">
+              <li>Products</li>
+              <li>Shops</li>
+              <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
